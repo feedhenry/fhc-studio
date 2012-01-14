6e37dfc68aa4ea5506b78e2d5d068514d117bb62
craig <craig.brookes@feedhenry.com>
2012-01-14 14:02:41 +0000

    improved rendering function and added apps page in dues

--- public/js/templates/templateRenderer.js
+++ public/js/templates/templateRenderer.js
@@ -1,62 +1,79 @@
 /**
- * Created by JetBrains WebStorm.
- * User: kelly
- * Date: 13/01/2012
- * Time: 21:29
- * To change this template use File | Settings | File Templates.
+This function renders the dust template with the data from
+ an ajax request.
+the links provide data about their intention. As in the provide a command in the element
+here in the class definitions. I like this structure because it maintains the pretty url.
+
+<a href="/apps" class="element:container template:apps content:json">apps</a>
+or in the url either is fine
+ <a href="/apps?el=container&t=apps"></a>
  */
 $('document').ready(function () {
 
-    var engine = renderer({});
+    var engine = renderer();
     $.get("/home", function (data) {
         engine.render({name:"home", 'data':data});
     });
 
-    $('a .singlepage').bind("click", function () {
-        var engine = renderer({path:$(this).attr("href")}).render()
-    });
+
 
 });
 
 
 var renderer = function (opts) {
-    var path        = opts.path || "/home",
-        template    = "",
+    var opts = opts || {},
+        path        =  opts.path || "/home",
         parse       = function () {
             var retPath;
             if(path === "/home"){ return "home";}
             else{
                 retPath = (path.substr(path.length -1) === "/") ? path.substr(1, path.length -1) : path.substr(1);
-                console.log(path);
-                console.log(path.split("/"));
+                console.log(retPath);
+                if(retPath.indexOf("/") === -1){
+                    return retPath;
+                }else{
+                    retPath = path.split("/");
+                    return retPath[0];
+                }
             }
-        };
+        },
+        template    = opts.template || parse(),
 
-        template = parse();
-        console.log(template);
+
         self = {
             render:function (params) {
                 var method  = params.method  || "get",
-                    cache   = params.cache   || false,
                     ele     = params.element || "container";
                 $.ajax({
-                    url:    path,
+                    url:    path+".json",
                     cache:  params.cache || false,
                     type:   method,
-                    success:function (html) {
-                        dust.render(params.name, params.data, function (err, out) {
+                    success:function (jdata) {
+                        console.log(jdata);
+                        console.log("template set to " +  template +" retdata "+ jdata);
+                        dust.render(template, jdata, function (err, out) {
                             if (err) {
                                 //err
+                                console.log(err);
                             }
                             else {
                                 console.log(params);
-                                console.log(out);
+
                                 $('body').html(out);
+
+                                $('body').find('.singlepage').click( function () {
+
+                                    console.log("clicked single page link");
+
+                                    var engine = renderer({path:$(this).attr("href")}).render({});
+                                    return false;
+                                });
                             }
                         });
                     },
                     error:  function () {
-
+                            console.log("Ajax error");
+                        return false;
                     }
                 });
 
