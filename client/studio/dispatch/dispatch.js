9366234530dcbde7fffd5ceb23fdbce9d387be2e
craig <craig.brookes@feedhenry.com>
2012-01-24 12:13:15 +0000

    new improved dispatch function

--- client/studio/dispatch/dispatch.js
+++ client/studio/dispatch/dispatch.js
@@ -1,42 +1,37 @@
 client.studio.dispatch = function () {
-    var self;
+    var self,container;
     //bind render to state change
     client.util.History.Adapter.bind(window, 'statechange', function () {
         var state = History.getState(),
             res, tpl, title, data;
-
         res = state.data;
         if (!res || !res.data) {
             return;
         }
-
-        tpl = res.template;
+        tpl = res.data.tpl;
         title = res.data.title;
         data = res.data;
-        self.render({'data':data, 'tpl':tpl});
+        console.log("res");
+        console.log(res);
+        //render dust template client side
+        dust.render(tpl, client.studio.helpers.dusthelpers.push(data), function (err, out) {
+            $(container).html(out);
+            //add update as a call back to the internal a href
+            // clicks
+            $(container).ajaxify(self.update);
+        });
     });
 
 
     self = {
         url:"",
-        update:function (path) {
+        update:function (path, opts) {
+            if(!opts) opts = {};
             self.url = (path === "/") ? "/home" : path;
-            $.get(path + ".json", self.render);
-        },
-
-        render:function (opts) {
-            var options = opts.data || {},
-                data = options.data || {},
-                tpl = options.tpl || {},
-                container = options.container || "#container";
-            //push state
-
-            client.util.History.pushState(options,options.title,self.url);
-
-            //render dust template client side
-            dust.render(tpl, client.studio.helpers.dusthelpers.push(options), function (err, out) {
-                $(container).html(out);
-                $(container).ajaxify(self.update);
+            container = opts.container || "#container";
+            //ajax call
+            $.get(self.url + ".json", function(data){
+                client.util.History.pushState(data,data.title,self.url);
             });
         }
     };
