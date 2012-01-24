59d90e5407b5b5ac5b96f1b8f0b86d2431dd87bc
craig <craig.brookes@feedhenry.com>
2012-01-24 12:24:20 +0000

    new improved dispatch function

--- client/studio/dispatch/dispatch.js
+++ client/studio/dispatch/dispatch.js
@@ -1,27 +1,5 @@
 client.studio.dispatch = function () {
     var self,container;
-    //bind render to state change
-    client.util.History.Adapter.bind(window, 'statechange', function () {
-        var state = History.getState(),
-            res, tpl, title, data;
-        res = state.data;
-        if (!res || !res.data) {
-            return;
-        }
-        tpl = res.data.tpl;
-        title = res.data.title;
-        data = res.data;
-        console.log("res");
-        console.log(res);
-        //render dust template client side
-        dust.render(tpl, client.studio.helpers.dusthelpers.push(data), function (err, out) {
-            $(container).html(out);
-            //add update as a call back to the internal a href
-            // clicks
-            $(container).ajaxify(self.update);
-        });
-    });
-
 
     self = {
         url:"",
@@ -33,11 +11,33 @@ client.studio.dispatch = function () {
             $.get(self.url + ".json", function(data){
                 client.util.History.pushState(data,data.title,self.url);
             });
+        },
+        render : function () {
+            var state = History.getState(),
+                res, tpl, title, data;
+            res = state.data;
+            if (!res || !res.data) {
+                return;
+            }
+            tpl = res.data.tpl;
+            title = res.data.title;
+            data = res.data;
+            //render dust template client side
+            dust.render(tpl, client.studio.helpers.dusthelpers.push(data), function (err, out) {
+                $(container).html(out);
+                //add update as a call back to the internal a href
+                // clicks
+                $(container).ajaxify(self.update);
+            });
         }
     };
+    //bind render to state change
+    client.util.History.Adapter.bind(window, 'statechange', self.render );
+
     return self;
 };
 
 $('document').ready(function () {
+
     client.studio.dispatch().update(window.location.pathname);
 });
