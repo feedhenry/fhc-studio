f256d701122f72aeb58e221a5d93c1955e491746
evanshortiss <evanshortiss@gmail.com>
2012-02-03 13:16:48 +0000

    Localisation changes to include branding

--- client/lang/en.js
+++ client/lang/en.js
@@ -1,5 +1,7 @@
 var branding = {
-    studioName : "Feedhenry"
+    studioName : "FeedHenry",
+    cli : "FHC",
+    apiPrefix : "$fh"
 }
 
 var en = { 
@@ -17,7 +19,6 @@ var en = {
   
   // Footer
   serviceStatus : "Service Status ",
-  studioName: "Feedhenry",
     
   // Apps Tab (apps.dust)
   name : "Name",
@@ -40,13 +41,19 @@ var en = {
   w3cid : "w3cid",
   guid : "guid",
   // App Editor (editor.dust)
+  close : "Close",
   file : "File",
   edit : "Edit",
   find : "Find",
+  newFile : "New",
   save : "Save",
   saveAll : "Save All",
+  saveAs : "Save As",
   saveClose: "Save & Close",
-  $fh : "$fh",
+  search : "Search...",
+  snippets: "Snippets",
+  someSnippets : "Some code snippets",
+  navigator : "Navigator",
   openRecent : "Open Recent",
   // App Preview (preview.dust)
   scale : "Scale",
@@ -67,22 +74,31 @@ var en = {
   // Home Screen (home.dust)
   appStudio : "App Studio",
   welcome : "Welcome",
-  getStarted : "Get started by installing FHC",
+  getStarted : "Get started by installing ",
   homeHeading : "Build apps powered by Cloud Foundry",
   stagingTo : "Staging to Cloud Foundry",
-  studioIntro : "Build powerful cloud powered apps with " + branding.studioName + ". Write in HTML5 and JavaScript. Deploy to your Cloud Foundry, and have a cloud scalable app on 5 platforms instantly!",
+  studioIntro : "Build powerful cloud powered apps with " +branding.studioName+ ". Write in HTML5 and JavaScript. Deploy to your Cloud Foundry, and have a cloud scalable app on 5 platforms instantly!",
   
   apiDocs : "API Docs",
-  apiText : "Read about the $fh APIs that exist to make calls to the FeedHenry cloud, or access on-device functionality not normally available to HTML5 apps.",
+  apiText : "Read about the " +branding.apiPrefix+ " APIs that exist to make calls to the " +branding.StudioName+ " cloud, or access on-device functionality not normally available to HTML5 apps.",
   readApiDocs : "Read the API Docs",
   
-  fhcDocs : "FHC Documentation",
-  fhcText : "Have a read of our documentation, where you can learn more about what Feedhenry is, and how to use our powerful cloud and client API's to build powerful mobile apps.",
-  readFhcDocs : "Read the FHC Docs",
+  cliDocs : branding.cli + " Documentation",
+  cliText : "Have a read of our documentation, where you can learn more about what " +branding.studioName+ " is, and how to use our powerful cloud and client API's to build powerful mobile apps.",
+  readCliDocs : "Read the "+branding.cli+" Docs",
   
   tutorials : "Tutorials",
   tutorialsText: "Learn how to utilize the cloud to hash then cache data from multiple sources in the cloud. Localize an app, cloud powered language assets make corrections and updates a breeze.",
-  viewTutorials : "View our tutorials" 
+  viewTutorials : "View our tutorials",
+  
+  
+  // API 7 Brand Related
+  studioName : branding.studioName,
+  cli : branding.cli,
+  apiAct: branding.apiPrefix + '.act',
+  apiCamera: branding.apiPrefix + '.camera',
+  apiWeb: branding.apiPrefix + '.web',
+  apiCache: branding.apiPrefix + '.cache'
 };
 
 module.exports = en;
