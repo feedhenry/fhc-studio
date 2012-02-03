9f80d155f5ceecc668105bb4bafd5f6fdad1ae58
evanshortiss <evanshortiss@gmail.com>
2012-02-03 10:31:54 +0000

    Structured language file by Studio Section (app.dust, editor.dust etc).

--- client/lang/en.js
+++ client/lang/en.js
@@ -1,38 +1,72 @@
 var en = { 
+  // Global Navbar 
+  reporting : "Reporting",
   admin : "Admin",
-  appDashboard : "App Dashboard",
-  apps : "Apps",  
+  home : "Home",
   appStudio : "App Studio",
+  dashboard : "Dashboard",
+  docs : "Docs",
+  help : "Help",
+  logout : "Logout",
+  prefs :  "Preferences",
+  apps : "Apps", 
+  
+  // Footer
+  serviceStatus : "Service Status ",
+  studioName: "Feedhenry",
+    
+  // Apps Tab (apps.dust)
+  name : "Name",
+  description : "Description",
+  modified : "Modified",
+  myapps : "My Apps",
+  version : "Version",
+    
+  
+  // App Dashboard (app.dust / dashboard.dust)
+  appDashboard : "App Dashboard",
+  editor : "Editor",
+  preview : "Preview",
   build : "Build",
   debug : "Debug",
-  dashboard : "Dashboard",
-  description : "Description",
-  docs : "Docs",
-  editor : "Editor",
-  email : "email",
-  forgotPassword : "Forgot...",
-  help : "Help",
-  home : "Home",
-  installing: "Installing",
-  login : "Login",
-  login_message : "Enter your username and password to login",
-  logout : "Logout",
-  modified : "Modified",
-  moreInfo : "More Info",
-  myapps : "My Apps",
   moreInfo : "moreInfo",
-  name : "Name",
-  password : "password",
-  prefs :  "Preferences",
-  preview : "Preview",
   previewOnMobile : "Preveiw on your mobile",
-  reporting : "Reporting",
-  serviceStatus : "Service Status ",
-  stagingTo : "Staging to",
-  version : "Version",
-  welcome : "Welcome"
   width : "width",
   height : "height",
+  w3cid : "w3cid",
+  guid : "guid",
+  // App Editor (editor.dust)
+  file : "File",
+  edit : "Edit",
+  find : "Find",
+  save : "Save",
+  saveAll : "Save All",
+  saveClose: "Save & Close",
+  $fh : "$fh",
+  openRecent : "Open Recent",
+  // App Preview (preview.dust)
+  scale : "Scale",
+  // App Build (build.dust)
+  build : "Build"
+  // App Debug (debug.dust)
+  debug: "Debug",
+ 
+  
+  // Login Screen (login.dust)
+  password : "password",
+  email : "email",
+  forgotPassword : "Forgot...",
+  login : "Login",
+  login_message : "Enter your username and password to login",
+  
+  
+  // Home Screen (home.dust)
+  appStudio : "App Studio",
+  welcome : "Welcome",
+  getStarted : "Get started by installing FHC"
+  stagingTo : "Staging to",
+  studioName: "Feedhenry",
+  studioIntro : "Build powerful cloud powered apps with {studioName}. Write in HTML5 and JavaScript. Deploy to your Cloud Foundry, and have a cloud scalable app on 5 platforms instantly!"
 };
 
 module.exports = en;
