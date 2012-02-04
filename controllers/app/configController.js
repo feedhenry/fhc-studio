0cf1de0276bdcc81eec400a0e42d4fd098d10bd4
skalee <skalee@gmail.com>
2012-02-04 10:40:40 +0100

    Configuration is being accessed.

--- controllers/app/configController.js
+++ controllers/app/configController.js
@@ -1,17 +1,24 @@
 var
     renderer = require("../../util"),
+    fhc      = require('fh-fhc'),
 
 configController = {
   indexAction: function(req, res, next) {
     var id = req.params.id;
-    var d = {
-        tpl: 'app',
-        title: 'Preferences',
-        appId: id,
-        data:{ inst : { guid : id}}, // TODO: This is same as appId - remove need for this!
-        tab: 'config'
-    };
-    renderer.doResponse(req, res, d);
+    fhc.configuration(['list', id], function (err, cfg) {
+      if (err) {
+        renderer.doError(res, req, "Couldn't list configuration for app"); return;
+      }
+      var d = {
+          tpl: 'app',
+          title: 'Preferences',
+          appId: id,
+          data:{ inst : { guid : id}}, // TODO: This is same as appId - remove need for this!
+          configuration: JSON.stringify(cfg),
+          tab: 'config'
+      };
+      renderer.doResponse(req, res, d);
+    });
   }
 };
 
