cedb43a23470c892b49ad6c6c023e004aacf3db8
Daniel Connor <danielconnr@gmail.com>
2012-04-25 18:59:56 +0100

    beginning work on dock

---
+++ client/studio/build/build.js
@@ -0,0 +1,30 @@
+client.studio = client.studio || {};
+client.studio.build = {
+
+  configs: [
+    "debug",
+    "distribution",
+    "release"
+  ],
+  destinations: [
+    "android", 
+    "iphone", 
+    "ipad",
+    "blackberry",
+    "windowsphone7"
+  ],
+
+  init: function() {
+    
+  },
+
+  bindEvents: function() {
+
+  },
+
+  build: function() {
+    $.post({
+
+    });
+  }
+}
