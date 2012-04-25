d8e17ac32959f448e77f9051256ff9b8a96afd6c
cianclarke <cian@cianclarke.com>
2012-04-25 22:12:18 +0100

    Most of the build logic to retrieve UI values

--- client/studio/build/build.js
+++ client/studio/build/build.js
@@ -15,13 +15,46 @@ client.studio.build = {
   ],
 
   init: function() {
+    this.bindEvents();
+  },
+  bindEvents: function() {
+    
+    $('.buildButton').unbind().on("click", this.buildHandler);
+  },
+  buildHandler: function(){
+    var me = client.studio.build;
+    // Do a build function according to the data-platform of the element
+    
+    var platform = $(this).data('platform'),
+    config = $('.' + platform + ' .config .active').text(),
+    version = $('.' + platform + ' .version .active').text();
+    
+    // iOS can be universal, iPhone or iPad..
+    if (platform=="iOS"){
+      platform = $('.iOS .target .active').text();
+    }
+    
+    var buildRequest = {
+        platform: platform,
+        config: config,
+        version: version
+    };
+    console.log(JSON.stringify(buildRequest));
+    
     
   },
-
-  bindEvents: function() {
-
+  buildiOS : function(){
+    
   },
-
+  buildAndroid : function(){
+    
+  },
+  buildBlackberry : function(){
+    
+  },
+  buildWP7 : function(){
+    
+  },
   build: function() {
     $.post({
 
