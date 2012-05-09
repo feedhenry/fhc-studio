ff7e93c706c2dcb7339056ec5a6d7ca40afff483
Daniel Connor <danielconnr@gmail.com>
2012-05-09 23:29:32 +0100

    trying drag drop

--- client/studio/account.js
+++ client/studio/account.js
@@ -9,21 +9,29 @@ client.studio.account = {
       self.uploadResource();
       e.preventDefault();
       return false;
-    }).on("dragenter", function(e) {
-      e.stopPropagation();
-      e.preventDefault();
-    }).on("dragexit", function(e) {
-      self.form.removeClass("drag-over");
-    }).on("dragover", function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      
-      self.form.addClass("drag-over");
-    }).on("drop", function(e) {
-      console.log(e.originalEvent.dataTransfer);
+    });
+    var formEl = this.form[0];
+
+    formEl.addEventListener("dragenter", function(e) {
 
       e.preventDefault();
-    });
+
+      e.dataTransfer.dropEffect = 'link';
+
+      return false;
+    }, false);
+    formEl.addEventListener("dragover", function(e) {
+      e.preventDefault();
+      e.dataTransfer.dropEffect = 'link';
+
+    }, false);
+
+    formEl.addEventListener("drop", function(e) {
+      console.log(e.dataTransfer.dropEffect);
+      e.preventDefault();
+      e.stopPropagation();
+
+    }, false);
 
   },
 
