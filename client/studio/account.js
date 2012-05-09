ef94d7c3dca30837d47b115b302b02e8943613a6
Daniel Connor <danielconnr@gmail.com>
2012-05-09 23:05:41 +0100

    implement uploading resources and started work on client side drag/drop resources

---
+++ client/studio/account.js
@@ -0,0 +1,38 @@
+client.studio = client.studio || {};
+client.studio.account = {
+  form: null,
+
+  init: function() {
+    var self = this;
+
+    this.form = $("form").unbind().on("submit", function(e) {
+      self.uploadResource();
+      e.preventDefault();
+      return false;
+    }).on("dragenter", function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+    }).on("dragexit", function(e) {
+      self.form.removeClass("drag-over");
+    }).on("dragover", function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      
+      self.form.addClass("drag-over");
+    }).on("drop", function(e) {
+      console.log(e.originalEvent.dataTransfer);
+
+      e.preventDefault();
+    });
+
+  },
+
+  uploadResource: function() {
+    var formEl = this.form[0];
+      formData = new FormData(formEl);
+
+    console.log(formData);
+
+  }
+
+};
